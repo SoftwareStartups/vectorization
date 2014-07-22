661b763611762525abec4071f966102969779208
Jos <Jos@192.168.2.206>
2014-07-22 17:47:15 +0200

    Created a float array for testing: value is indeed same as C version. Updated console prints.

--- jsrc/JavaNativeVector/src/com/vectorfabrics/JavaNativeVector.java
+++ jsrc/JavaNativeVector/src/com/vectorfabrics/JavaNativeVector.java
@@ -16,11 +16,21 @@ public class JavaNativeVector {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        vectorizeLib top = new vectorizeLib();
-        String vname = top.vectorizedVersionName();
-        System.out.println("Hello me");
-        System.out.println(vname);
-
-    }
+        vectorizeLib veclib = new vectorizeLib();
+        String vname = veclib.vectorizedVersionName();
     
+        System.out.println("Hello, I do " + vname);
+
+        int n = 512;
+        float[] a = new float[n];
+        for (int i=0; i<n; i++) {
+            a[i] = 1.0f + (float)(i & 0x0f);
+        }
+        
+        float r1 = veclib.vectorizedInproduct(n, a, a);
+        float r2 = veclib.vectorizedAverage(n, a);
+        
+	System.out.println("inprod = " + Float.toString(r1));
+	System.out.println("average = " + Float.toString(r2));
+    }
 }
